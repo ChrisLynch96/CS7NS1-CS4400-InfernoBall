8e85cdacf303f0e09397435955db528723455d89
ChrisLynch96 <chlynch@tcd.ie>
2018-11-05 14:58:27 +0000

    the script I used to split up the wordlist

---
+++ Layer9/split_wordlist.py
@@ -0,0 +1,23 @@
+
+
+with open("crackstation-human-only.txt", "r+") as wordlist:
+    words = wordlist.read().splitlines()
+
+def chunkIt(seq, num):
+    avg = len(seq) / float(num)
+    out = []
+    last = 0.0
+
+    while last < len(seq):
+        out.append(seq[int(last):int(last + avg)])
+        last += avg
+
+    return out
+
+splits = chunkIt(words, 4)
+
+#writing to files
+for i in range(len(splits)):
+    with open("wordlist%d.txt" % i, "w+") as outlfile:
+        for j in range(len(splits[i])):
+            outlfile.write("%s\n" % splits[i][j])
